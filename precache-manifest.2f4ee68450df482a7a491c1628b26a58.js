48e4ea56fa2b9beb950cf974867f1c4a807e6f2c
Mrityunjay Kumar <53057286+mrityunjay-1@users.noreply.github.com>
2020-10-29 07:36:31 +0530

    Updates

---
+++ precache-manifest.2f4ee68450df482a7a491c1628b26a58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484528fc8f26e01e48287377932fb1c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c881851b558eb57cefe9",
+    "url": "/static/css/main.12cc3ce4.chunk.css"
+  },
+  {
+    "revision": "024772ce4f667f8b01d0",
+    "url": "/static/js/2.49f70a1b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.49f70a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c881851b558eb57cefe9",
+    "url": "/static/js/main.2748fdae.chunk.js"
+  },
+  {
+    "revision": "7f42403ec663b31222d7",
+    "url": "/static/js/runtime-main.d596cfc0.js"
+  },
+  {
+    "revision": "2410bd35f85287ac216c0d1f93f1ae96",
+    "url": "/static/media/logo.2410bd35.png"
+  },
+  {
+    "revision": "5a843f124a31d38957e60617067db086",
+    "url": "/static/media/logo2.5a843f12.png"
+  }
+]);
